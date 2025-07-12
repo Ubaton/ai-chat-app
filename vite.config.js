4828a7e22eebcb833fd50b4637df9ef93932410b
Raymond <32289508+Ubaton@users.noreply.github.com>
2025-07-12 19:15:26 +0200

    Build error fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,4 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
-});
+export default defineConfig({ plugins: [react(), TanStackRouterVite()] });
